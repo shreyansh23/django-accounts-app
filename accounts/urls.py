2d085290d66906b4de11aa6da414b26f40ce2bd8
Shreyansh Jain <iitrhackers@gmail.com>
2018-06-06 01:31:01 +0530

    Login-form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import login
 urlpatterns = [
     path('', views.home),
+    path('login/', login, {'template_name':'accounts/login.html'})
 ]
