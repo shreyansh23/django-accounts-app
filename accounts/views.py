65ce2b40043ee283c485d163be4c2f98f3b5a06d
Shreyansh Jain <iitrhackers@gmail.com>
2018-06-05 23:26:45 +0530

    video 7, jinja templating (for loop), accesing values from views in templates

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render, HttpResponse
 #from django.http import HttpResponse,
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/login.html')
+    name = "Shreyansh"
+    numbers = [1,2,3,4,5]
+    args = {'myName':name, 'myNumbers':numbers}
+
+    return render(request, 'accounts/login.html',args)
