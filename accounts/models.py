ef6a7ea8a76701d6a31c854d3547ae0db54e2682
Shreyansh Jain <iitrhackers@gmail.com>
2018-06-06 03:38:52 +0530

    video 11, admin panil activated

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    website = models.URLField(default='')
+    phone = models.IntegerField(default='')
